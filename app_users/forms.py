d2a71882becd51ca577d67377e104da7451e8bf8
Harshal Vibhute <Vibhuteha1598@gmail.com>
2021-09-30 12:54:50 +0530

    Updated With new Templates

--- app_users/forms.py
+++ app_users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import fields
 from app_users.models import UserProfileInfo
 
 class UserForm(UserCreationForm):
@@ -33,3 +34,21 @@ class UserProfileInfoForm(forms.ModelForm):
     class Meta():
         model = UserProfileInfo
         fields = ('bio', 'profile_pic', 'user_type')
+
+class User_Form(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'username', 
+            'first_name', 
+            'last_name', 
+            'email', 
+        ]
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfileInfo
+        fields = [
+            'bio',
+            'profile_pic',
+        ] 
--- app_users/views.py
+++ app_users/views.py
@@ -1,13 +1,20 @@
+from django.contrib.auth import models
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from app_users.forms import UserForm, UserProfileInfoForm
+from django.urls.base import reverse_lazy
+from django.views.generic.edit import FormView
+from app_users.forms import UserForm, UserProfileInfoForm,User_Form,ProfileForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import REDIRECT_FIELD_NAME, authenticate, login, logout,update_session_auth_hash
 from django.views.generic import TemplateView
 from curriculum.models import Standard
-from .models import UserProfileInfo, Contact
-from django.views.generic import CreateView
+from .models import  UserProfileInfo, Contact
+from django.views.generic import CreateView,UpdateView
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 def user_login(request):
     if request.method == "POST":
@@ -84,3 +91,48 @@ class ContactView(CreateView):
     model = Contact
     fields = '__all__'
     template_name = 'app_users/contact.html'
+
+def change_pass(request):
+    if request.method == "POST":
+        changpass_form=PasswordChangeForm(user=request.user, data=request.POST)
+        if changpass_form.is_valid() :
+            changpass_form.save()
+            messages.success(request,'Password changed Successfuly!')
+            update_session_auth_hash(request, changpass_form.user)
+            return HttpResponseRedirect(reverse_lazy('change_pass'))
+
+    else:
+        changpass_form=PasswordChangeForm(user=request.user)
+    return render(request,'app_users/changepass.html',{'changpass_form':changpass_form})
+
+class ViewProfile(TemplateView):
+    template_name = 'app_users/profile.html'
+
+class ProfileUpdateView(LoginRequiredMixin, TemplateView):
+    user_form = User_Form
+    profile_form = ProfileForm
+    template_name = 'app_users/profile-update.html'
+
+    def post(self, request):
+
+        post_data = request.POST or None
+        file_data = request.FILES or None
+
+        user_form = User_Form(post_data, instance=request.user)
+        profile_form = ProfileForm(post_data, file_data, instance=request.user.userprofileinfo)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return HttpResponseRedirect(reverse_lazy('profile'))
+
+        context = self.get_context_data(
+                                        user_form=user_form,
+                                        profile_form=profile_form
+                                    )
+
+        return self.render_to_response(context)     
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
